db04a37afac5abeafeee8519b96eb8c2a81a7fab
Giancarlo Vera <paulterry2001@hotmail.com>
2016-08-01 03:16:43 -0300

    Title module added for each page in our app

--- js/home/index.js
+++ js/home/index.js
@@ -1,7 +1,8 @@
 var page = require('page');
+var title = require('title');
 
 page('/', function(ctx, next){
+	title('Platzigram')
 	var main = document.getElementById('mainContainer');
-
 	main.innerHTML = '<a href="/signup">Signup</a>';	
 })
--- js/signin/index.js
+++ js/signin/index.js
@@ -1,9 +1,10 @@
 var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
+var title = require('title');
 
 page('/signin', function(ctx, next){
+	title('Platzigram | Signin')
 	var main = document.getElementById('mainContainer');
-
 	empty(main).appendChild(template);
 })
